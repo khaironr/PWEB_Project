80f201bc54b6f26fef81d562a923a274df8ad86d
khaironr <87608400+khaironr@users.noreply.github.com>
2021-07-18 21:18:46 +0700

    Update Fungsi

---
+++ cek.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_SESSION['log']))
+{
+
+}
+else
+{
+    header('location:index.php');
+}
+?>
---
+++ fungsi.php
@@ -0,0 +1,61 @@
+<?php
+
+//membuat koneksi ke database
+//baca komentnya ki
+
+//tambahan di database di tabel masuk dan keluar 1 tabel
+//tabel qty doang tipenya int sisanya g usah diisi
+//klik save
+$conn = mysqli_connect("localhost","root","","stockbarang2");//angka 2 dihapus
+session_start(); //untuk yang sudah pernah login
+
+//menambah barang baru
+
+if(isset($_POST['addnewbarang']))
+{
+    $namabarang = $_POST['namabarang'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+
+    $addtotable = mysqli_query($conn, "insert into stock (namabarang, deskripsi, stock) values('$namabarang','$deskripsi','$stock')");
+    //pastikan penulisannya benar
+    //tadi di insert kurang t, hasilnya tdk keluar
+};
+
+
+//barang masuk
+if(isset($_POST['barangmasuk']))
+{
+    $barangnya = $_POST['barangnya'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $cekstocksekarang = mysqli_query($conn, "select * from stock where idbarang='$barangnya'");
+    $ambildatanya = mysqli_fetch_array($cekstocksekarang);
+
+    $stocksekarang = $ambildatanya['stock'];
+    $tambahkanstocksekarangdenganquantity = $stocksekarang+$qty;
+
+    $addtomasuk = mysqli_query($conn, "insert into masuk (idbarang, keterangan, qty) values('$barangnya','$penerima','$qty')");
+    $updatestockmasuk = mysqli_query($conn, "update stock set stock='$tambahkanstocksekarangdenganquantity' where idbarang='$barangnya'");
+    
+};
+
+//barang keluar
+if(isset($_POST['addbarangkeluar']))
+{
+    $barangnya = $_POST['barangnya'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $cekstocksekarang = mysqli_query($conn, "select * from stock where idbarang='$barangnya'");
+    $ambildatanya = mysqli_fetch_array($cekstocksekarang);
+
+    $stocksekarang = $ambildatanya['stock'];
+    $tambahkanstocksekarangdenganquantity = $stocksekarang+$qty;
+
+    $addtomasuk = mysqli_query($conn, "insert into keluar (idbarang, penerima, qty) values('$barangnya','$penerima','$qty')");
+    $updatestockmasuk = mysqli_query($conn, "update stock set stock='$tambahkanstocksekarangdenganquantity' where idbarang='$barangnya'");
+};
+
+?>
